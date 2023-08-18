1d2857120c1ffd52ce1a35321d323af6f0405ef2
AZZIE2000 <azzam.faraj0@gmail.com>
2023-08-19 00:19:45 +0300

    azzie

--- src/services/Builder.ts
+++ src/services/Builder.ts
@@ -1,7 +1,20 @@
 abstract class Builder {
   constructor(protected text: string) {}
   protected abstract preparePrompt(): void;
-  protected abstract fst(): void;
-  protected abstract filterBuild(): void;
-  public build(): any {}
+  protected abstract fillJSON(): void;
+  public build(): any {
+    this.preparePrompt();
+    this.fillJSON();
+    return this.generateURL();
+  }
+  protected abstract generateURL(): any;
+}
+
+abstract class CarsBuilder extends Builder {
+  preparePrompt(): void {
+    this.text = this.text;
+  }
+  protected fillJSON(): void {
+    this.text = "cars";
+  }
 }
