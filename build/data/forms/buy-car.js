87d215e4ec84be73a9f9a3fd08422bbd299a8ff4
AZZIE2000 <azzam.faraj0@gmail.com>
2023-09-09 17:18:05 +0300

    update

--- build/data/forms/buy-car.js
+++ build/data/forms/buy-car.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.buyCar = void 0;
-exports.buyCar = {
+exports.formToFill = void 0;
+exports.formToFill = {
     car_brand: null,
     car_model: null,
     car_year_from: null,
     car_year_to: null,
-    car_type: null,
+    car_body_type: null,
     car_price_from: null,
     car_price_to: null,
     car_regional_specs: null,
     car_transmission: null,
-    car_fuel: null,
+    // car_fuel: null,
     car_color: null,
     car_interior_options: null,
     car_condition: null,
@@ -21,7 +21,8 @@ exports.buyCar = {
     car_license_status: null,
     car_insurance_status: null,
     car_payment_method: null,
-    car_manifacture_country: null,
+    car_manufacture_country: null,
     car_listing_city: null,
     car_listing_region: null,
+    lang: null,
 };
